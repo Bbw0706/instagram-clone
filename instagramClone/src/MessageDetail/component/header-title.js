2dd920a7799799bb13be5017648d6ac4b4fe3bc0
Bbw0706 <bbwsama@gmail.com>
2018-08-30 22:26:56 +0700

    fixing header messageDetail

--- instagramClone/src/MessageDetail/component/header-title.js
+++ instagramClone/src/MessageDetail/component/header-title.js
@@ -11,13 +11,13 @@ import {
 const HeaderTitle = ({navigation}) => {
 	const {item} = navigation.state.params
     return (
-    	<View>
+    	<View style={{width:"100%", alignItems:"center", right:22}}>
 			<Thumbnail 
 				small 
 				source={{uri: 'https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_960_720.png'}}
-				style={{borderColor:"#ccc", left:113, borderWidth:2}}
+				style={{borderColor:"#ccc", borderWidth:2}}
 			/>
-	    	<Text style={{fontSize:12, left:90}}>{item.name}</Text>
+	    	<Text style={{fontSize:12}}>{item.name}</Text>
     	</View>
     )
 }
